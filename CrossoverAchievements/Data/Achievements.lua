a88b3ea7e701096ac700dcef020a1c5ba9400919
javier himura <javier_himura@hotmail.com>
2022-09-18 15:07:11 +0200

    Fix categories and achievements data for achievements that don't exits on current version

--- CrossoverAchievements/Data/Achievements.lua
+++ CrossoverAchievements/Data/Achievements.lua
@@ -16,9 +16,14 @@ function Achievements:SetAchievementData(achievementid, points, accountwide)
 		return;
 	end;
 	if not points and not accountwide then
-		local flags;
-		_, _, points, _, _, _, _, _, flags, _, _, _, _, _, _ = GetAchievementInfo(achievementid);
-		accountwide = bit.band(flags, ACHIEVEMENT_FLAGS_ACCOUNT) == ACHIEVEMENT_FLAGS_ACCOUNT;
+		local id, flags;
+		id, _, points, _, _, _, _, _, flags, _, _, _, _, _, _ = GetAchievementInfo(achievementid);
+		if id ~= nil then
+			accountwide = bit.band(flags, ACHIEVEMENT_FLAGS_ACCOUNT) == ACHIEVEMENT_FLAGS_ACCOUNT;
+		else
+			points = 0;
+			accountwide = false;
+		end
   	end;
 	Achievements.List[achievementid] = {};
 	Achievements.List[achievementid].Points = points;
@@ -26,13 +31,15 @@ function Achievements:SetAchievementData(achievementid, points, accountwide)
 	Achievements.List[achievementid].NextID = GetNextAchievement(achievementid);
 	Achievements.List[achievementid].PreviousID = GetPreviousAchievement(achievementid);
 	local categoryid = GetAchievementCategory(achievementid);
-    Achievements.List[achievementid].CategoryID = categoryid;
-	CrossoverAchievements.Data.Categories:SetCategoryAchievement(categoryid, achievementid);
+	if categoryid ~= nil then
+		Achievements.List[achievementid].CategoryID = categoryid;
+		CrossoverAchievements.Data.Categories:SetCategoryAchievement(categoryid, achievementid);
+	end
 end
 
 function Achievements:GetAchievementData(achievementid) 
-	if Achievements.List[achievementid] then
+	if not Achievements.List[achievementid] then
 		Achievements:SetAchievementData(achievementid);
 	end;
-	return Achievements.List[achievementid];
+	return Achievements.List[achievementid] 
 end
