c3bb4e9365fe44a6c92ee243ef86cdc308ad13c0
javier himura <javier_himura@hotmail.com>
2022-09-19 19:43:23 +0200

    Track achievements completed by other characters without needing to login with all characters first

--- CrossoverAchievements/CrossoverAchievements.lua
+++ CrossoverAchievements/CrossoverAchievements.lua
@@ -24,6 +24,8 @@ CrossoverAchievements.AddonDataVersion = 2;
 local Blz_AchievementFrame_ToggleAchievementFrame = nil;
 local GetAchievementInfo = GetAchievementInfo;
 local ClearWTFData = false;
+local CharacterNames = nil;
+local SaveCharacterNamesWTF = true;
 
 function CrossoverAchievements:OnInitialize()
     if not self.GameVersion:IsValidVersion() then
@@ -255,36 +257,36 @@ end
 
 function CrossoverAchievements:GetCurrentCharacterTable()
     local playerGUID = UnitGUID("player");
-    local GameVersionTable = self:GetCurrentGameVersionTable();
-    local CharacterTable = GameVersionTable.Characters[playerGUID];
+    local CurrentGameVersionTable = self:GetCurrentGameVersionTable();
+    local CharacterTable = CurrentGameVersionTable.Characters[playerGUID];
     
     if CharacterTable == nil then
         CharacterTable = {};
     	CharacterTable.Time = time();
         CharacterTable.Achievements = CharacterTable.Achievements or {};
-        CharacterTable.version = AddonDataVersion;
-        CharacterTable.GUID = playerGuid;
+        CharacterTable.GUID = playerGUID;
         CharacterTable.Realm = GetRealmName();
         CharacterTable.Name = UnitName("player");
         CharacterTable.GameVersion = self.GameVersion:GetServerType();
-        GameVersionTable.Characters[playerGUID] = CharacterTable;
+        CurrentGameVersionTable.Characters[playerGUID] = CharacterTable;
     end
 
-    return GameVersionTable.Characters[playerGUID];
+    return CurrentGameVersionTable.Characters[playerGUID];
 end
 
 function CrossoverAchievements:ExportAchievements()
-    local GameVersionTable = self:GetCurrentGameVersionTable();
-    local CharacterTable = self:GetCurrentCharacterTable();
+    local CurrentGameVersionTable = self:GetCurrentGameVersionTable();
+    local CurrentCharacterTable = self:GetCurrentCharacterTable();
     local categories = GetCategoryList();
+    self:GroupCharactersByName(CurrentGameVersionTable);
     for categoryindex,categoryID in ipairs(categories) do
         local numAchievements = GetCategoryNumAchievements(categoryID);
         for index = 1, numAchievements do
             local achievementid = select(1, GetAchievementInfo(categoryID, index));
-            self:ExportAchievement(GameVersionTable, CharacterTable, achievementid);
+            self:ExportAchievement(CurrentGameVersionTable, CurrentCharacterTable, achievementid);
             local previousid = GetPreviousAchievement(achievementid)
             while previousid ~= nil do
-                self:ExportAchievement(GameVersionTable, CharacterTable, previousid);
+                self:ExportAchievement(CurrentGameVersionTable, CurrentCharacterTable, previousid);
                 previousid = GetPreviousAchievement(previousid)
             end
         end
@@ -292,34 +294,123 @@ function CrossoverAchievements:ExportAchievements()
 end
 
 function CrossoverAchievements:OnAchievementEarned(achievementid)
-    local GameVersionTable = self:GetCurrentGameVersionTable();
-    local CharacterTable = self:GetCurrentCharacterTable();
-    self:ExportAchievement(GameVersionTable, CharacterTable, achievementid);
-    local accountachievement = CrossoverAchievements.API.IsAccountWideAchievement(achievementid);
-	self.Account:ProcessCompletedAchievement(achievementid, time(), accountachievement, true, true, CharacterTable.Name,  CharacterTable.Realm, CharacterTable.GameVersion);
+    local CurrentGameVersionTable = self:GetCurrentGameVersionTable();
+    local CurrentCharacterTable = self:GetCurrentCharacterTable();
+    self:ExportAchievement(CurrentGameVersionTable, CurrentCharacterTable, achievementid);
+    local accountachievement = self.API.IsAccountWideAchievement(achievementid);
+	self.Account:ProcessCompletedAchievement(achievementid, time(), accountachievement, true, true, CurrentCharacterTable.Name,  CurrentCharacterTable.Realm, CharacterTable.GameVersion);
     self.Data.Achievements:RefreshSortAchievement(achievementid);
+    self.Data.LastAchievements:SetLastAchievement(achievementid, time());
 end
 
-function CrossoverAchievements:ExportAchievement(GameVersionTable, CharacterTable, achievementid)
+function CrossoverAchievements:ExportAchievement(CurrentGameVersionTable, CurrentCharacterTable, achievementid)
     local _, achname, points, completed, achivementmonth, achivementday, achivementyear, _, flags, _, _, isGuild, wasEarnedByMe, earnedBy, isStatistic = GetAchievementInfo(achievementid);
     if completed and not isGuild and not isStatistic then
         local achievementtime = time({year = 2000 + achivementyear, month = achivementmonth, day = achivementday});
         if ( bit.band(flags, ACHIEVEMENT_FLAGS_ACCOUNT) == ACHIEVEMENT_FLAGS_ACCOUNT ) then
-            CrossoverAchievements.Data.Achievements:SetAchievementData(achievementid, points, true);
-			if not GameVersionTable.Achievements[achievementid] or GameVersionTable.Achievements[achievementid] ~= achievementtime then
-                GameVersionTable.Achievements[achievementid] = achievementtime;
-                GameVersionTable.Time = time();
+            -- Account achievement added directly to the game version table
+            self.Data.Achievements:SetAchievementData(achievementid, points, true);
+			if not CurrentGameVersionTable.Achievements[achievementid] or CurrentGameVersionTable.Achievements[achievementid] ~= achievementtime then
+                CurrentGameVersionTable.Achievements[achievementid] = achievementtime;
+                CurrentGameVersionTable.Time = time();
             end
         elseif wasEarnedByMe then
-            CrossoverAchievements.Data.Achievements:SetAchievementData(achievementid, points, false);
-            if not CharacterTable.Achievements[achievementid] or CharacterTable.Achievements[achievementid] ~= achievementtime then
-                CharacterTable.Achievements[achievementid] = achievementtime;
-                CharacterTable.Time = time();
-                GameVersionTable.Time = time();
+            -- Character achievement earned by the current character
+            self.Data.Achievements:SetAchievementData(achievementid, points, false);
+            if not CurrentCharacterTable.Achievements[achievementid] or CurrentCharacterTable.Achievements[achievementid] ~= achievementtime then
+                CurrentCharacterTable.Achievements[achievementid] = achievementtime;
+                CurrentCharacterTable.Time = time();
+                CurrentGameVersionTable.Time = time();
+
+                -- We check if the achievement was added before to a table indexed by name in the else case 
+                self:RemoveAchievementByName(CurrentGameVersionTable, CurrentCharacterTable, achievementid, achievementtime);
             end
         else
-            CrossoverAchievements.Data.Achievements:SetAchievementData(achievementid, points, false);
-            --print('Achievement ' .. achname .. ' earned by ' .. earnedBy);
+            -- Character achievement earned by the another character
+            -- we don't know guid nor realm so we search for an already tracked character with that achievement
+            -- in case we don't know the character we add the data to a temporal table indexed by character name
+            -- it will be removed in the wasEarnedByMe condition once the player login with that character
+            self.Data.Achievements:SetAchievementData(achievementid, points, false);
+            self:ExportAchievementByName(CurrentGameVersionTable, CurrentCharacterTable, achievementid, earnedBy, achievementtime); 
         end
     end
+end
+
+function CrossoverAchievements:GroupCharactersByName(CurrentGameVersionTable)
+    if CharacterNames ~= nil then
+	    return;
+	end
+    CharacterNames = {};
+    -- Create a temporal table of characters grouped by name, to obtain any possible character that has earned an achievement
+    -- when blizzard's data indicate an achievement was earnedBy a character without realm nor guid
+    -- the player could have character with the same name on different realms
+
+    for index,CharacterTable in pairs(CurrentGameVersionTable.Characters) do 
+        if CharacterTable and CharacterTable.GUID then
+            if CharacterNames[CharacterTable.Name] == nil then
+                CharacterNames[CharacterTable.Name] = {};
+                CharacterNames[CharacterTable.Name].Guids = {};
+                CharacterNames[CharacterTable.Name].Total = 0;
+			end
+            CharacterNames[CharacterTable.Name].Total = CharacterNames[CharacterTable.Name].Total + 1;
+            CharacterNames[CharacterTable.Name].Guids[CharacterNames[CharacterTable.Name].Total] = CharacterTable.GUID;
+		end
+    end
+    if SaveCharacterNamesWTF then   
+        CrossoverAchievements_AccountData.CharacterNames = CharacterNames;
+	else
+        CrossoverAchievements_AccountData.CharacterNames = nil;
+	end
+end
+
+function CrossoverAchievements:ExportAchievementByName(CurrentGameVersionTable, CurrentCharacterTable, achievementid, earnedBy, achievementtime)
+	-- Achievement from other character obtained from blizzard's data, we don't know character guid nor realm
+	if earnedBy == nil or earnedBy == "" then
+	    return;
+	end
+    -- Search through all characters with guid and the same name
+	local CharacterByNameTable = CurrentGameVersionTable.Characters[earnedBy];
+    if CharacterNames[earnedBy] ~= nil then
+        for _,CharacterGuid in pairs(CharacterNames[earnedBy].Guids) do
+            local CharacterGuidTable = CurrentGameVersionTable.Characters[CharacterGuid];
+            if CharacterGuidTable and 
+			   CharacterGuidTable.Achievements[achievementid] and 
+			   CharacterGuidTable.Achievements[achievementid] == achievementtime then
+                -- we have found the character by guid, so we don't need to add the achievement
+                if CharacterByNameTable ~= nil then
+                    -- if we already added the achievement to a character indexed by name we remove it
+                    self:RemoveAchievementByName(CurrentGameVersionTable, CharacterByNameTable, achievementid, achievementtime);
+				end
+                return;
+			end
+        end
+	end
+    -- we haven't found the character that earned the achievement so we create a temporal table with character name instead of guid
+    -- if player login with that character later the temporal table will be removed
+    if CharacterByNameTable == nil then
+        CharacterByNameTable = {};
+    	CharacterByNameTable.Time = time();
+        CharacterByNameTable.Achievements = {};
+        CharacterByNameTable.Name = earnedBy;
+        CharacterByNameTable.GameVersion = self.GameVersion:GetServerType();
+        CurrentGameVersionTable.Characters[earnedBy] = CharacterByNameTable;
+    end
+
+    if not CharacterByNameTable.Achievements[achievementid] or CharacterByNameTable.Achievements[achievementid] ~= achievementtime then
+		CharacterByNameTable.Achievements[achievementid] = achievementtime;
+        CharacterByNameTable.Time = time();
+        CurrentGameVersionTable.Time = time();
+    end
+end
+
+function CrossoverAchievements:RemoveAchievementByName(CurrentGameVersionTable, CurrentCharacterTable, achievementid, achievementtime)
+    -- Remove achievement from temporal table by character name
+    -- the name has been added in the table by character guid
+	local CharacterByNameTable = CurrentGameVersionTable.Characters[CurrentCharacterTable.Name];
+    if CharacterByNameTable and 
+	   CharacterByNameTable.Achievements[achievementid] and 
+       CharacterByNameTable.Achievements[achievementid] == achievementtime
+	then
+        CharacterByNameTable.Achievements[achievementid] = nil;
+	end
 end
