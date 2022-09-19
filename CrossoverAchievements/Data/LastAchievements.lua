86caec8dc0ed875185a722270ef4b51870ecfd18
javier himura <javier_himura@hotmail.com>
2022-09-19 19:33:10 +0200

    New functions to calculate LastAchievements

--- CrossoverAchievements/Data/LastAchievements.lua
+++ CrossoverAchievements/Data/LastAchievements.lua
@@ -3,7 +3,10 @@ local CrossoverAchievements = LibStub("AceAddon-3.0"):GetAddon("CrossoverAchieve
 CrossoverAchievements.Data = CrossoverAchievements.Data or {};
 CrossoverAchievements.Data.LastAchievements = CrossoverAchievements.Data.LastAchievements or {};
 
+
 local LastAchievements = CrossoverAchievements.Data.LastAchievements;
+local GetCategoryList = GetCategoryList;
+ 
 local List = {};
 local Total = 0;
 local LastTime = nil;
@@ -19,6 +22,21 @@ function LastAchievements:SetLastAchievement(achievementid, achievementime)
 		table.remove(List, MaxLastAchievements + 1);
 	else
 		Total = Total + 1;
+	end
+end
+
+function LastAchievements:SetLastAchievements()
+	List = {};
+	Total = 0;
+	LastTime = nil;
+	for _,categoryid in pairs(GetCategoryList()) do
+		for index = 1, CrossoverAchievements.API.GetCategoryNumAchievements(categoryid) do
+			local achievementid = CrossoverAchievements.API.GetAchievementInfo(categoryid, index);
+			local AccountInfo = CrossoverAchievements.Account:GetCompletedAchievementInfo(achievementid);
+			if AccountInfo then 
+				self:SetLastAchievement(achievementid, AccountInfo.AchievementTime);
+			end
+		end
 	end
 end
 
